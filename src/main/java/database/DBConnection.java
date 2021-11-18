c0f6e7aa4cf70ef78331dee30527067c1695aea4
Michele Delli Paoli <m.dellipaoli1@studenti.unisa.it>
2021-11-18 09:44:17 +0100

    Creation Class DBConnection

--- src/main/java/database/DBConnection.java
+++ src/main/java/database/DBConnection.java
@@ -1,4 +1,36 @@
 package main.java.database;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class DBConnection {
+
+    private String url;
+    private String user;
+    private String password;
+
+    public DBConnection(){
+        this.url = "jdbc:postgresql://localhost:5432/postgres";
+        this.user = "postgres";
+        this.password = "";
+    }
+
+    public DBConnection(String url, String user, String password){
+        this.url = url;
+        this.user = user;
+        this.password = password;
+    }
+
+    public Connection getConnetion() throws ClassNotFoundException, SQLException {
+        Class.forName("org.postgresql.Driver");
+
+        //Connessione al Database
+        Connection conn = DriverManager.getConnection(url, user, password);
+
+        return conn;
+    }
+
+
+
 }
